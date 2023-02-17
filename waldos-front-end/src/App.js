f2196e3606f6228eb5d5d5203bb19dfcd6560e49
Michael-Mehta <supermichael2001@gmail.com>
2023-02-17 13:06:14 -0700

    rails api is connected with react however still some bugs

--- waldos-front-end/src/App.js
+++ waldos-front-end/src/App.js
@@ -2,18 +2,58 @@
 import './App.css';
 import imageTwo from './components/Waldo-Two.jpg'
 import GameBoard from './components/GameBoard';
+import axios from "axios";
+import { useEffect, useState } from 'react';
+
+const API_URL = "http://localhost:3000/api/v1/characters";
+
+
+
+function getAPIData(){
+  return axios.get(API_URL).then((response) => response.data)
+}
+
+
+
 
 function App() {
+
+  const [characters, setCharacters] = useState([])
+const [foundWaldo, setFoundWaldo] = useState(false)
+
+useEffect(() => {
+
+  let mounted = true;
+
+  getAPIData().then((person) => {
+
+    if(mounted)
+    {
+      setCharacters(person)
+    }
+
+  });
+
+
+  return () => (mounted = false);
+
+
+}, []);
+
   return (
     <div className="App">
-      <h1>Welcome Challenger</h1>
+      {foundWaldo ? <h1>Winner!!!!!!!!</h1> : <h1>Welcome Challenger</h1>
+      }
       <p>In order to beat this game you must find 
         all the characters in the picture by clicking on them
         at the end your time will be recorded and you will be placed
         on a leaderboard</p>
         <img src = {imageTwo} alt = "Waldo" className='main-image'/>
       <div>
-     < GameBoard />
+     < GameBoard
+      characters = {characters}
+      foundWaldo = {foundWaldo}
+      setFoundWaldo = {setFoundWaldo}/>
      </div>
     </div>
   );
--- waldos-front-end/src/components/GameBoard.js
+++ waldos-front-end/src/components/GameBoard.js
@@ -4,17 +4,28 @@ import imageTwo from './Waldo.png'
 
 import '../App.css';
 
-function GameBoard() {
+function GameBoard({props}) {
   const [firstArray, setFirstArray] = useState([...Array(34)])
   const [secondArray, setSecondArray] = useState([...Array(23)])
 
-  const ToggleItem = ({ id }) => {
+  const ToggleItem = ({ id, x, y}) => {
     const [toggleThisElement, setToggleThisElement] = useState(false);
     const [toggleDropdown, setToggleDropdown] = useState(false);
     const dropdown = () => {
       setToggleThisElement(!toggleThisElement)
       setToggleDropdown(!toggleDropdown)
+      console.log(x)
+      console.log(y)
+      {props.characters.map((character) =>
+        
+        {(character.xAxis === x && character.yAxis === y) ?
+            props.setFoundWaldo(true): props.setFoundWaldo(false)}
+    
+      
+        
+        )}
 
+      
     }
     
     return (
@@ -44,7 +55,9 @@ function GameBoard() {
           {
       secondArray.map((e, i) => (
        
-        <ToggleItem id = {i} />
+        <ToggleItem id = {i}
+        x = {a} 
+        y = {i}/>
        
       ))
 }
